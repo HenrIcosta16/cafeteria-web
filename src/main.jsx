9eb78acd6849dfe1e08ef3eac5a172754348c72b
HenrIcosta16 <154642352+HenrIcosta16@users.noreply.github.com>
2024-08-28 18:16:00 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,23 @@
 import ReactDOM from 'react-dom/client';
+import Router from './App.jsx';
+import {  RouterProvider } from "react-router-dom";
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider  router={Router} />
+  </React.StrictMode>
+);
+
+
+/* antes
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
@@ -13,3 +32,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 );
+*/
