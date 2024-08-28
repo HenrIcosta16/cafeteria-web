e95f5838d5c95f351ca54b0a1163aeed3708908b
HenrIcosta16 <154642352+HenrIcosta16@users.noreply.github.com>
2024-08-28 18:05:03 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,34 @@
 import './App.css';
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './templates/Layout';
+import NoPage from './views/NoPage';
+import Home from './views/Home';
+import Cardapio from './views/Cardapio';
+import Login from './views/Login';
+
+//modelo de rotas atualizado
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    elementError: <NoPage />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: 'cardapio', element: <Cardapio /> },
+      { path: 'login', element: <Login /> }, 
+    ]
+  },
+]);
+
+
+export default Router;
+
+
+
+
+/*codigo anterior
+
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './templates/Layout';
 import Home from './views/Home';
@@ -6,8 +36,11 @@ import NoPage from './views/NoPage';
 import Cardapio from './views/Cardapio';
 import Login from './views/Login';
 
+
 function App() {
   return (
+
+    //metodo antigo
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
@@ -22,3 +55,4 @@ function App() {
 }
 
 export default App;
+*/
